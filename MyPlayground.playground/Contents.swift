14b38716117df783ca091d1b457e50f7f0470077
Drake Wright <rdrakewright@gmail.com>
2023-01-17 09:09:39 -0600

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+print("Drake")
+// Int, String, Float, Double, Bool, and Character
+// The LET variable type
+// Let (AKA Constant) is used for variables with a value that will NOT change
+var person = "Chris"
+// Cannot reassign/update the value of a LET variable
+person = "Robert"
+print(person) // Chris
+
+//bonus
+var number = 3
+
+number = number+2
+
+number += 5
+//same as above
+print(number)
+
+var nums :Array = [ 1 , 8 , 9]
